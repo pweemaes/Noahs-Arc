de9dce54ec6d71ac11682d1225d8f7bf0bee37dd
pweemaes <pweemaes@college.harvard.edu>
2015-04-19 22:08:35 -0400

    messing

---
+++ Noahs-Arc/Constraint9.java
@@ -0,0 +1,22 @@
+
+/** Constraint9
+ */
+public class Constraint9
+{
+    private Variable[] scope;
+
+    public Constraint9(Variable v1, Variable v2, Variable v3, 
+                      Variable v4, Variable v5, Variable v6,
+                      Variable v7, Variable v8, Variable v9)
+    {
+    }
+
+    public boolean check() 
+    { return false;
+    }
+    
+    public int getValue(int variablePosition)
+    { return 0;
+    }
+    
+}
